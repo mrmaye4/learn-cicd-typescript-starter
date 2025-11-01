d8a5fed800f1af8aff28f94175cd8f1e5b13e5b2
mrmaye4 <mrmaye4@gmail.com>
2025-11-01 19:28:36 +0200

    Refactor ESLint config for improved clarity and alignment with best practices

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 import pluginSecurity from "eslint-plugin-security";
-import { CompatibleConfigArray } from "typescript-eslint/dist/compatibility-types";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+    },
     ignores: ["node_modules/*", "dist/*", "coverage/*"],
   },
-  tseslint.configs.recommended,
-  pluginSecurity.configs.recommended as CompatibleConfigArray,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
